55223f734fef119c51c5f209f93185bde1c5bd51
Ruchi <ruchi.thanabhawan@gmail.com>
2024-09-21 22:34:42 -0700

    add images

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div>
-      <section className="py-20 text-center md:text-left">
+      <section className="py-32 text-center md:text-left">
         <div className="max-w-screen-xl mx-auto px-4 flex flex-col md:flex-row items-center mt-8 ml-12">
           <div className="md:w-1/2">
             <h1 className="text-6xl font-bold">
@@ -23,10 +23,12 @@ const HeroSection = () => {
                 Start for free
               </button>
               <button className="py-2.5 px-6 border rounde border-gray-200">
-               How it's works?
+                How it's works?
               </button>
             </div>
-            <p className="text-green-600">Trusted by over 8M sellers around the world</p>
+            <p className="text-green-600">
+              Trusted by over 8M sellers around the world
+            </p>
           </div>
           <div className="md:w-1/2 mt-8 md:mt-0">
             <img
@@ -34,6 +36,44 @@ const HeroSection = () => {
               alt="Print-on-demand illustration"
               className="w-80 boreder border-2 rounded-xl p-4 h-auto"
             />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-8 mt-60">
+          <div className="text-center">
+            <img
+              src="https://printify.com/pfh/assets/legacy/higher-profits.svg"
+              alt="Higher Profits Icon"
+              className="w-28 mx-auto mb-8"
+            />
+            <h1 className="text-xl font-bold mb-4">Higher Profits</h1>
+            <p className="text-gray-600 max-w-xs mx-auto">
+              We offer some of the lowest prices in the industry because print
+              providers continuously compete to win your business.
+            </p>
+          </div>
+          <div className="text-center">
+            <img
+              src="https://printify.com/pfh/assets/legacy/robust-scaling.svg"
+              alt="Robust Scaling Icon"
+              className="w-28 mx-auto mb-8"
+            />
+            <h1 className="text-xl font-bold mb-4">Robust Scaling</h1>
+            <p className="text-gray-600 max-w-xs mx-auto">
+              Easily handle peak holiday seasons, with our wide network of
+              partners and automatic routing functionality.
+            </p>
+          </div>
+          <div className="text-center">
+            <img
+              src="https://printify.com/pfh/assets/legacy/best-selection.svg"
+              alt="Best Selection Icon"
+              className="w-28 mx-auto mb-8"
+            />
+            <h1 className="text-xl font-bold mb-4">Best Selection</h1>
+            <p className="text-gray-600 max-w-xs mx-auto">
+              With 900+ products and top quality brands, you can choose the best
+              products for your business.
+            </p>
           </div>
         </div>
       </section>
